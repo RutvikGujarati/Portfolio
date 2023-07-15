d5998e2748c1cf12127cde715d49b3dfeedd189d
Rutvik Gujarati <125241992+RutvikGujarati@users.noreply.github.com>
2023-07-15 10:36:43 +0530

    Update Handles.jsx

--- src/components/handles/Handles.jsx
+++ src/components/handles/Handles.jsx
@@ -1,1 +1,20 @@
+//import React from 'react'
+import './Handles.css'
+import { AiFillLinkedin, AiFillTwitterSquare } from 'react-icons/ai';
+import { FaGithubSquare } from 'react-icons/fa';
+
+const Handles = () => {
+  return (
+    <section className='socials'>
+      <a href="https://www.linkedin.com/in/rutvik-gujarati-b46989223/" target='_blank' rel="noopener noreferrer"><AiFillLinkedin className='icon' /></a>
+      <a href="https://twitter.com/GhostThe00" target='_blank' rel="noopener noreferrer"><AiFillTwitterSquare className='icon' /></a>
+      <a href="https://github.com/RutvikGujarati" target='_blank' rel="noopener noreferrer"><FaGithubSquare className='icon' />
+      </a>
+
+
+    </section>
+  )
+}
+
+export default Handles
 
